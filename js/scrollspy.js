3d7786cf82a5bd8e7c1e9b9dfe2521cccf265f9d
Miki Tucto Leon <mick01leon@gmail.com>
2023-05-24 20:41:24 -0500

    arreglando el scrollspy en el video inteligente

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -5,14 +5,12 @@ export default function scrollSpy(){
     //root
     //si  a rootMargin damos margenes negativos funcionara casi como el threshold
     // rootMargin: '10px 20px 30px 40px',
-    threshold: [0.7, 0.7]
+    threshold: [0.8, 0.8]
   }
   const $sections= d.querySelectorAll(".section")
   const $indexSections=d.querySelectorAll(".menu a")
   const observador=new IntersectionObserver((entradas)=>{
-  
     if(entradas[0].isIntersecting){
-  
       $indexSections.forEach((el)=>{
         if(el.getAttribute("href").substring(1)===entradas[0].target.getAttribute("id")){
           el.classList.add("scroll-focus")
--- js/video_inteligente.js
+++ js/video_inteligente.js
@@ -2,7 +2,7 @@ const d=document,
   w=window
 export default function videoInteligente(){
   const options={
-    threshold: 0.95
+    threshold: 1
   }
   const cb=(entradas)=>{
     entradas.forEach(el=>{
@@ -10,21 +10,21 @@ export default function videoInteligente(){
         el.target.play()
       }else{
         el.target.pause()
-      }  
-      
+      }
+
       w.addEventListener("visibilitychange",()=>{
         if(d.visibilityState=="visible"){
           if(el.isIntersecting){
             el.target.play()
           }else{
             el.target.pause()
-          } 
+          }
         }else{
           el.target.pause()
         }
       })
     })
-  
+
   }
   const $videos=d.querySelectorAll("video[data-smart-video]")
   const observador= new IntersectionObserver(cb,options)
